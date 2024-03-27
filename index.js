c2a0ff70ebe79642a758b86f067cfd71e8f392ee
root <root@SURFACE.>
2024-03-27 08:24:25 +0300

    events

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+const dodger=getElementById("dodger");
+
+
+function moveDodgerRight(){
+    let rightHand =dodger.style.left.replace("px","");
+    let left =parseInt(rightHand,10);
+    if(left<360){
+        dodger.style.left=`${left+1}px`;
+    }
+    }
+    function moveDodgerLeft(){
+        let leftHand =dodger.style.left.replace("px","");
+        const left =parseInt(leftHand,10);
+        if(left>0){
+            dodger.style.left=`${left-1}px`;
+        }
+        }
+    
+
